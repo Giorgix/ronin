87323ab2ab2298c8b04dc341bfc156445486e010
Giorgix <jackosinapsis@gmail.com>
2014-02-16 23:44:22 -0500

    first commit with folders for the enviroment and first elements of the library and base and app-example level

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
+css_dir = "static/css/"
+sass_dir = "static/sass/"
 images_dir = "images"
-javascripts_dir = "js"
+javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -16,9 +16,4 @@ javascripts_dir = "js"
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+preferred_syntax = :sass
